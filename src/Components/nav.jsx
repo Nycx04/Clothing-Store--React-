ea8823d5b07431627ab6130bae9f3ded0e6c55b3
john dave cordoto <johndavecordoto04@gmail.com>
2026-03-25 14:14:18 +0800

    Nav file update

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -1,11 +1,15 @@
-import './nav.css'
+import './nav.css';
+import Icon from '../assets/Dialogue-Dirge-Chibi-Icon.png';
 
 function Nav() {
     return(
     <div id="black-tile">
         <h1>Clothier</h1>
-        <input type="search"></input>
+        <input type="search" placeholder='Search'></input>
+        <a href="#Home">Home</a>
         <a href="#Discover">Discover</a>
+        <a href="#Cart">🛒</a>
+        <a href="#Profile"><img src={Icon}></img></a>
     </div>
     );
 }
