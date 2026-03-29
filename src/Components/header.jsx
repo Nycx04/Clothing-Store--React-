f206e1c2f8d24bbb02bf91f06375dc3f47d24701
john dave cordoto <johndavecordoto04@gmail.com>
2026-03-29 23:56:08 +0800

    card upd

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -5,7 +5,6 @@ import Card from './trendingcards.jsx';
 function Header () {
     return (
         <div id="Trending">
-        <h1>You might like!</h1>
         <div id="Box">
             <Card/>
         </div>
--- src/Components/trendingcards.jsx
+++ src/Components/trendingcards.jsx
@@ -1,10 +1,11 @@
 import './trendingcards.css';
+import Card1 from '../assets/photo-1529636273736-fc88b31ea9d9.avif';
 
 function Card(){
     return(
         <div id="cards">
         <div id="card1">
-            
+        <img src={Card1} ></img>
         </div>
         </div>
     );
