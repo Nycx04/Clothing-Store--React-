12853817edb7688f4e7e2a1227b94b55152f9531
john dave cordoto <johndavecordoto04@gmail.com>
2026-03-31 00:24:35 +0800

    cards upd

--- src/Components/trendingcards.jsx
+++ src/Components/trendingcards.jsx
@@ -3,17 +3,35 @@ import Card1 from '../assets/photo-1529636273736-fc88b31ea9d9.avif';
 
 function Card(){
     return(
-        <div id="cards">
-        <div id="card1">
-        <img src={Card1}/>
-        <h1>White Dress</h1>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. </p>
+        <div id="layout">
+        <div className="Cards">
+        <div id="card1" className="cards">
+            <img src={Card1}/>
+        <div id="card-text" className="card-text">
+            <h1>White Dress</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis iste perferendis </p>
         </div>
-        <div id="card2">
-
         </div>
-        <div id="card3">
+        </div>
 
+        <div className="Cards">
+        <div id="card2" className="cards">
+            <img src={Card1}/>
+        <div id="card-text" className="card-text">
+            <h1>White Dress</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis iste perferendis </p>
+        </div>
+        </div>
+        </div>
+
+        <div className="Cards">
+        <div id="card3" className="cards">
+            <img src={Card1}/>
+        <div id="card-text" className="card-text">
+            <h1>White Dress</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis iste perferendis </p>
+        </div>
+        </div>
         </div>
         </div>
     );
