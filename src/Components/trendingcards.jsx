29e391ec66dde92456727a66213b1431f1f20f85
john dave cordoto <johndavecordoto04@gmail.com>
2026-03-30 21:52:23 +0800

    nothing

--- src/Components/trendingcards.jsx
+++ src/Components/trendingcards.jsx
@@ -2,20 +2,19 @@ import './trendingcards.css';
 import Card1 from '../assets/photo-1529636273736-fc88b31ea9d9.avif';
 
 function Card(){
-    const cardData = [
-    { title: "White Dress", body: "$200",  image: "photo-1529636273736-fc88b31ea9d9.avif" },
-    { title: "Red Jacket",  body: "$150",  image: "premium_photo-1675186049366-64a655f8f537.avif" },
-    ];
-
     return(
         <div id="cards">
-        {cardData.map((card, i) => (
-        <div key={i} className="card">
-        <img src={card.image} alt={card.title} className="card-img" />
-        <div className="card-title">{card.title}</div>
-        <div className="card-body">{card.body}</div>
+        <div id="card1">
+        <img src={Card1}/>
+        <h1>White Dress</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. </p>
         </div>
-        ))}
+        <div id="card2">
+
+        </div>
+        <div id="card3">
+
+        </div>
         </div>
     );
 }
