6b7333025afd311b67e2d2d5818208bf453a066a
john dave cordoto <johndavecordoto04@gmail.com>
2026-04-01 20:20:29 +0800

    type

--- src/Components/trendingcards.jsx
+++ src/Components/trendingcards.jsx
@@ -1,6 +1,7 @@
 import './trendingcards.css';
 import Card1 from '../assets/photo-1529636273736-fc88b31ea9d9.avif';
-
+import Card2 from '../assets/premium_photo-1675186049366-64a655f8f537.avif';
+import Card3 from '../assets/photo-1685354218016-3899c9ef79ad.avif';
 function Card(){
     return(
         <div id="layout">
@@ -10,26 +11,29 @@ function Card(){
         <div id="card-text" className="card-text">
             <h1>White Dress</h1>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis iste perferendis </p>
+            <button>Buy Now</button>
         </div>
         </div>
         </div>
 
         <div className="Cards">
         <div id="card2" className="cards">
-            <img src={Card1}/>
+            <img src={Card2}/>
         <div id="card-text" className="card-text">
-            <h1>White Dress</h1>
+            <h1>Red Coat</h1>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis iste perferendis </p>
+            <button>Buy Now</button>
         </div>
         </div>
         </div>
 
         <div className="Cards">
         <div id="card3" className="cards">
-            <img src={Card1}/>
+            <img src={Card3}/>
         <div id="card-text" className="card-text">
-            <h1>White Dress</h1>
+            <h1>Green Hoodie</h1>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis iste perferendis </p>
+            <button>Buy Now</button>
         </div>
         </div>
         </div>
