7efd72d04061b887a321f0f948b787c165bd02d8
john dave cordoto <johndavecordoto04@gmail.com>
2026-04-10 23:16:55 +0800

    idk

--- src/Components/main.jsx
+++ src/Components/main.jsx
@@ -1,28 +1,36 @@
 import './main.css';
+import { useState } from 'react';
 
 function Main(){
-    const Itemlist = [
+    const [ItemList, setItemlist] = useState([
         {
+            id:1,
             img:Img1,
             title:"Boots",
             Desc:"lorem ipsum"
         },{
+            id:2,
             img:Img2,
             title:"T shirt",
             Desc:"lorem ipsum"
         },{
+            id:3,
             img:Img3,
             title:"Sweatshirt",
             Desc:"Sweatshirt ni patrick"
         },{
+            id:4,
             img:Img4,
             title:"V-neck",
             Desc:"V-neck ni lance"
         },{
+            id:5,
             img:Img5,
             title:"Hoddie",
             Desc:"Hoodie ni kevs"
-        }];
+        }
+    ]);
+
         return(
         <div id="Main">
         <div id="display">
